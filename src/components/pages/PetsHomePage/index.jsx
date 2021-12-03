36c9721ea716cbcd14232a33c8b576d717017db0
ivyjing <ivy.caoj@gmail.com>
2021-12-02 19:16:18 -0500

    add new pet function added

--- src/components/pages/PetsHomePage/index.jsx
+++ src/components/pages/PetsHomePage/index.jsx
@@ -59,7 +59,7 @@ export const PetsHomePage = () => {
 
   const getPets = async() => {
     try {
-      const response = await fetch('https://firestore.googleapis.com/v1/projects/pets-api-40916/databases/(default)/documents/pets/');
+      const response = await fetch('https://firestore.googleapis.com/v1/projects/it-tf-123/databases/(default)/documents/pets/');
       const data = await response.json();
       console.log(data);
       const formattedData =data.documents.map((item) => {
